f96e0bf74727b10b510d6b9a1f002cf021c3954c
Aldaeva <80864888+AldaEvaSaputri@users.noreply.github.com>
2021-06-02 14:18:09 +0700

    Add files via upload

---
+++ Loginuser/src/loginuser/Loginuser.java
@@ -0,0 +1,19 @@
+//Nama  : Alda Eva Saputri
+//NIM   : 11200930000022
+//Kelas : 2A Sistem Informasi
+package loginuser;
+
+/**
+ *
+ * @author AldaEva
+ */
+public class Loginuser {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
